30e6266c8505dfb4a88a80e1741f382285597c7f
Mariia Tyshchenko <122963239+M-Tyshchenko@users.noreply.github.com>
2023-08-07 12:27:53 +0300

    Update FriendListItem.js
    delete className

--- src/components/FriendListItem/FriendListItem.js
+++ src/components/FriendListItem/FriendListItem.js
@@ -3,9 +3,9 @@ import { Status } from './FriendListItem.styled';
 export const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <>
-      <Status className="status" status={isOnline}></Status>
-      <img className="avatar" src={avatar} alt="User avatar" width="48" />
-      <p className="name">{name}</p>
+      <Status status={isOnline}></Status>
+      <img src={avatar} alt="User avatar" width="48" />
+      <p>{name}</p>
     </>
   );
 };
